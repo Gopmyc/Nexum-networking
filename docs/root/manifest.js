f997abc16c6bbc6c8b0ad0aaa5b8d58040b008d1
Gopmyc <hoyaux.gaspard@gmail.com>
2026-03-02 00:47:29 +0100

    chore(root/manifest.js): 🔧 - Updated file (manifest.js)

---
+++ docs/root/manifest.js
@@ -0,0 +1,28 @@
+const manifest = {
+		"NETWORKING": {
+				"CLIENT.html": null,
+				"LIBRARIES": {
+						"CLIENT": {
+								"EVENTS": {
+										"CONNECT.html": null,
+										"DISCONNECT.html": null,
+										"RECEIVE.html": null,
+										"SEND.html": null
+								}
+						},
+						"CODEC.html": null,
+						"EVENTS.html": null,
+						"HOOKS.html": null,
+						"SERVER": {
+								"EVENTS": {
+										"CONNECT.html": null,
+										"DISCONNECT.html": null,
+										"RECEIVE.html": null,
+										"SEND.html": null,
+										"UNGANDLED.html": null
+								}
+						}
+				},
+				"SERVER.html": null
+		}
+};
